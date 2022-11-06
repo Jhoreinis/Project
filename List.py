3a2654572e3fb6fe43f6b78e3da9381fa8b0ee7d
sbonadiez <101202112+sbonadiez@users.noreply.github.com>
2022-11-06 18:16:16 -0500

    Update List.py

--- List.py
+++ List.py
@@ -1,1 +1,35 @@
+from Info import Info    
+import io
+class List:   
+    def _init_(self):
+        self.Head = None
+        self.ULT = None
+        self.contador = 0          
+    def append(self, data=''):  
+        P = Info(data)
+        if self.Head == None:
+            self.Head = P
+            return
+        last = self.Head
+        while last.next:    
+            last = last.next
 
+        last.next = P
+        
+    def WriteList(self):
+        Info
+        P = self.Head
+        while(P != None):
+            print(P.data, end="->")
+            P = P.next
+            contador+=1
+        print("None")
+    
+    def _repr_(self):
+        respuesta = ""
+        P = self.Head
+        while(P != None):
+            respuesta = respuesta + str(P.data) + "->"
+            P = P.next
+        respuesta = respuesta + "None"
+        return respuesta
