54359fa8374d75dd2750ee6e7a08a1e903bc26be
Jhoreinis <91376980+Jhoreinis@users.noreply.github.com>
2022-11-06 18:15:18 -0500

    Update CodetoRun.py

--- CodetoRun.py
+++ CodetoRun.py
@@ -4,9 +4,7 @@ linkedlist = List()
 with open('BaseDate.txt', 'r') as I:
     for line in I:
         linkedlist.append(line.strip())   
-
-#Imprmir Lista Simple Enlazada 
-print(linkedlist) #Imprimir Lista con peliculas recomendadas 
+print(linkedlist) 
 
         
         
