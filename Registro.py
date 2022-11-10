697a8d5305d96b79333f06212f89ceca4ce3d9d3
sbonadiez <101202112+sbonadiez@users.noreply.github.com>
2022-11-10 08:29:06 -0500

    Update Registro.py

--- Registro.py
+++ Registro.py
@@ -1,41 +1,32 @@
 class Registro:
     
-    def __init__(self, nombre, contraseña ) -> None:
-        self.nombre = nombre
+    def _init_(self, usuario: str, contraseña: str): 
+        self.usuario = usuario
         self.contraseña = contraseña
-        self.numUsuarios = 0
         self.conectar = False
-        self.intento = 3
-        
-        
-        Registro.numUsuarios+=1
+        self.intento = 3  
 
-    def login(self):
-        nombreUsuario = input("Ingrese nombre de usuario:")
-        contraseñaUsuario = input("Ingrese contraseña:")
-        if contraseñaUsuario == self.contraseña and nombreUsuario == self.nombre:
-            print ("Ha iniciado sesión con exito")
-            self.conectar = True
-        else:
-            self.intento-=1
-            if self.intento > 0:
-              print ("Contraseña o nombre de usuario incorrectos, intentelo nuevamente")
-              self.login()
+    def registrar(self, usuario: str, contraseña: str) -> bool:
+        with open("registro_usuario.txt", "w") as archv:
+            registro = usuario + "," + contraseña + "\n"
+            archv.write(registro)
+            #archv.close()
+        
+    def inisioSesion(self, usuario: str, contraseña: str) -> bool:
+        archv = open("registro_usuario.txt", "w")
+        archv.read()
+        for linea in archv:
+            user, contra = linea.split(",")
+            if user == usuario and int (contra) == contraseña:
+                print ("Ha iniciado sesión con exito")
+                self.conectar = True    
+            elif self.intento > 0:
+                self.intento-=1
+                print ("Contraseña o nombre de usuario incorrectos, intentelo nuevamente")
+                self.inisioSesion()
             else:
-              print ("no se pudo iniciar sesión, excedio numero de intentos")
-
-            
-    def close(self):
-        if self.conectar:
-            print("cerro sesión con exito")
-            self.conectar = False
-            
-user = Registro(input("Registre nombre de usuario"), input("Crea una contraseña"))
-print(user)
-
-user.login()
-print(user)
-
+                print ("no se pudo iniciar sesión, excedio numero de intentos")
+        return True
 
 
 
