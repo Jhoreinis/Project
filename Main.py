5c19dd0ff464a367f0b89d7e43fc5a37bc862baf
sbonadiez <101202112+sbonadiez@users.noreply.github.com>
2022-11-11 21:34:11 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,40 +1,18 @@
-from List import List
-from Registro import Registro
+from BuscarPelis import BuscarPelis
 
-print("Bienvenido\n 1.Registrarse\n 2.Iniciar sesión\n 3.salir")
-opcion = int(input("Ingrese opción:\n"))
-while opcion:
-    while opcion!= 1 and opcion !=2 and opcion !=3:
-            print("Opción incorrecta.\n Elija una opción nuevamente")
-            print("1.Registrarse\n 2.Iniciar sesión\n 3.salir")
-            opcion = int(input("Ingrese opción:\n"))
-            
-    if opcion  == 1:
-        usuario = (input("Cree un nombre de usuario:\n"))
-        contraseña = (input("Digite una contraseña:\n"))
-        Registro.registrar = (usuario, contraseña)
-        registro = Registro.registrar
-        print ("Registro exitoso")
-        opcion = False
-        
-    elif opcion == 2:
-        user = input("ingrese nombre de usuario:\n")
-        contra = input("ingrese contraseña:\n")
-        Registro.inisioSesion = (user, contra)
-        login = Registro.inisioSesion
-        print ("Ha iniciado sesión con exito")
-        opcion = False
-    else:
-        print("Ha salido con exito")
-        break
-
-#linkedlist = List()
-#with open('BaseDate.txt', 'r') as I:
-    #for line in I:
-        #linkedlist.append(line.strip())   
-#print(linkedlist) 
-
-#linkedlist.recomendar_categoria()
-
+interes1 = input("Ingrese primera categoria de interes:")  
+interes2 = input("Ingrese segunda categoria de interes:")
+interes3 = input("Ingrese tercera categoria de interes:")
+#search = borrador(interes1, interes2, interes3)
+#search.buscar()
+buscador = BuscarPelis (interes1, interes2, interes3)
+while BuscarPelis.buscar(buscador) == False:
+    print(BuscarPelis.buscar(buscador))
+    print('Los géneros ingresados no son correctos')
+    interes1 = input("Ingrese primera categoria de interes:")  
+    interes2 = input("Ingrese segunda categoria de interes:")
+    interes3 = input("Ingrese tercera categoria de interes:")
+    buscador = BuscarPelis (interes1, interes2, interes3) 
+buscador.printMoviebyCat()
         
         
