1d4b40aa3fbcc5e843aa8ab764498fdb55f5572b
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2022-11-14 00:04:37 -0500

    Update Registro.py

--- Registro.py
+++ Registro.py
@@ -6,51 +6,99 @@ class Registro:
         self.conectado = False
         self.intento = 3    
         
+        """
+        Los atributos usuario y contraseña son 
+        necesarios para que existan usuarios y 
+        pasar como parametro a cada metodo
+        """
         
     def usuarioRegistrado(self):
-        file = open("BaseRegistro.txt", "r")
-        line = file.readlines()
+        """
+        Se abre el archivo que contiene la informacion
+        de cada usuario como lectura
+        """
+        file2 = open("BaseRegistro.txt", "r")
+        lines2 = file2.readlines()
+        """
+        Se utiliza un contador para verificar 
+        que el usuario ya existe.cada vez que encuentre un usuario
+        que ya existe aumenta el contador
+        """
         conta = 0
-        for linea in line:
-            user = linea.split(',')[0]
+        for linea2 in lines2:
+            user = linea2.split(',')[0]
             if self.usuario == user:
                 conta +=1 
-        file.close()
+        file2.close()
+        """
+        Si el contador es mayor a 1
+        quiere decir que ya hay un usuario registrado
+        con ese nombre y retornara falso. si retorna False
+        le pedirá al usuario que ingrese nuevamente un usuario diferente
+        """
         if conta >= 1:
             return False
         return True 
     
     def registrar(self):
-        file = open("BaseRegistro.txt", "a")
+        
+        """
+        Cuando el contador de arriba retorne True el usuario 
+        se ha registrado con exito y sus datos han sido guardados
+        """
+        
+        file2 = open("BaseRegistro.txt", "a")
         registro = (self.usuario + "," + self.contraseña)
-        file.write(registro + "\n")
+        file2.write(registro + "\n")
         print("Registro exitoso")
-        file.close() 
+        file2.close() 
     
     def verificarLogin(self):
-        file = open("BaseRegistro.txt", "r")
-        lines = file.readlines()
-        for linea in lines:
-            save_split = linea.split(',')
-            user, contraseña, = save_split[0], save_split[1]
+        
+        """
+        Abrimos nuevamente el archivo como lectura
+        para verificar que el usuario haya ingresado
+        correctamente los datos al momento de iniciar sesión
+        """
+        
+        """
+        Se utiliza split para poder separar y comparar 
+        el usuario ingresado con el usuario anteriormente registrado
+        asi mismo se hace con la contraseña. si retorna True la 
+        información ingresada es correcta y el usuario podrá ingresar
+        """
+        
+        file2 = open("BaseRegistro.txt", "r")
+        lines2 = file2.readlines()
+        for linea2 in lines2:
+            save_split2 = linea2.split(',')
+            user, contraseña, = save_split2[0], save_split2[1]
             if (self.usuario ==  user):
                 return True
             if (self.contraseña == contraseña):
                 return True
-        file.close()
+        file2.close()
         if (self.usuario != user and self.contraseña != contraseña):
             return False
     
+    
     def login(self):
-        file = open("BaseRegistro.txt", "r")
-        line = file.readlines()
-        for linea in line:
-            save_split = linea.split(',')
-            user, contraseña, = save_split[0], save_split[1]
+        
+        """
+        Luego de verificar anteriormente los datos 
+        utilizamos este metodo para ingresar oficialmente
+        al sistema.
+        """
+        
+        file2 = open("BaseRegistro.txt", "r")
+        lines2 = file2.readlines()
+        for linea2 in lines2:
+            save_split2 = linea2.split(',')
+            user, contraseña, = save_split2[0], save_split2[1]
             if self.usuario == user or self.contraseña == contraseña:
                 print("Ha iniciado sesion con exito")
                 self.conectado = True
-        file.close() 
+        file2.close() 
     
     
     
