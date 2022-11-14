1d1f5370484792c23f0c8806796920be86385b38
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2022-11-14 15:06:07 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,6 @@
-from BuscarPelis import BuscarPelis
+from Menu import Menu
 
-interes1 = input("Ingrese primera categoria de interes:")  
-interes2 = input("Ingrese segunda categoria de interes:")
-interes3 = input("Ingrese tercera categoria de interes:")
-#search = borrador(interes1, interes2, interes3)
-#search.buscar()
-buscador = BuscarPelis (interes1, interes2, interes3)
-while BuscarPelis.buscar(buscador) == False:
-    print(BuscarPelis.buscar(buscador))
-    print('Los géneros ingresados no son correctos')
-    interes1 = input("Ingrese primera categoria de interes:")  
-    interes2 = input("Ingrese segunda categoria de interes:")
-    interes3 = input("Ingrese tercera categoria de interes:")
-    buscador = BuscarPelis (interes1, interes2, interes3) 
-buscador.printMoviebyCat()
+menu = Menu()
+menu.inicio()
         
         
